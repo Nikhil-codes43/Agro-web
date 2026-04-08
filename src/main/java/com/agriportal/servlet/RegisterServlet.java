32032a9a354ae0a0d7198b85dbfe15795659d37c
Nikhil Kumar Patidar <nikhilpatidar584@gmail.com>
2026-04-08 13:14:01 +0530

    Update RegisterServlet.java

--- src/main/java/com/agriportal/servlet/RegisterServlet.java
+++ src/main/java/com/agriportal/servlet/RegisterServlet.java
@@ -11,35 +11,28 @@ import com.agriportal.dao.User;
 import com.agriportal.dao.UserDao;
 import com.agriportal.dao.UserDaoImpl;
 
-
+@WebServlet("/RegisterServlet")   // ✅ YE ADD KARNA HAI
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final UserDao userDao = new UserDaoImpl();
-       
-   
-    public RegisterServlet() {
-        super();
-        
-    }
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
-			String email = request.getParameter("email");
-			
-			User user = new User();
-			user.setUsername(username);
-			user.setPassword(password);
-			user.setEmail(email);
-			
-			
-			if(userDao.addUser(user)) {
-				response.sendRedirect("login.jsp?registration=success");
-			}else {
-				response.sendRedirect("signup.jsp?error=1");
-			}
-	}
 
+		System.out.println("🔥 Servlet hit hua");  // DEBUG
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setEmail(email);
+
+		if(userDao.addUser(user)) {
+			response.sendRedirect("login.jsp?registration=success");
+		}else {
+			response.sendRedirect("signup.jsp?error=1");
+		}
+	}
 }
